b065a7f2aa79b7190044ad047f1f92f9ead07aa1
Adela Spirkova <ad.spirkova@gmail.com>
2020-07-15 13:23:51 +0200

    Add links

---
+++ Animation/script.js
@@ -0,0 +1,8 @@
+// Definuj hodnotu casu jako "new Date"
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // vyber v html dokumentu element podle id 
+    // a pridej tomu text (innerText), ktery bude odpovidat hodnote casu
+
+});
